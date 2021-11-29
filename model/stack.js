873d1be5e82dc3eda59dfec6f3dcc8b74e8c55fd
Lucas Bernardes <lbeurncaradess@tutanota.com>
2021-11-28 21:03:31 -0300

    Objeto Stack criado

---
+++ model/stack.js
@@ -0,0 +1,50 @@
+function Stack() {
+  // Atributos Privados
+  let count = 0
+  let items = {}
+
+  //#region Métodos Públicos
+  this.push = _element => {
+    items[count] = _element
+    count++
+  }
+
+  this.pop = () => {
+    if(this.isEmpty())
+      return undefined
+    
+    this.count--
+    let result = items[count]
+    delete items[count]
+    return result
+  }
+
+  this.size = () => count
+
+  this.isEmpty = () => count === 0
+
+  this.peek = () => {
+    if(this.isEmpty())
+      return undefined
+
+    items[count - 1]
+  }
+
+  this.clear = () => {
+    while(!this.isEmpty())
+      this.pop()
+  }
+
+  this.toString = () => {
+    if(this.isEmpty())
+      return ''
+    
+    let objString = `${items[0]}`
+
+    for(let i = 0; i < count; i++)
+      objString = `${objString}, ${items[i]}`
+
+    return objString
+  }
+  //#endregion
+}
