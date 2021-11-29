1dd53aa967283392ddfb4da14c1a28523d20babd
Lucas Bernardes <lbeurncaradess@tutanota.com>
2021-11-28 22:02:28 -0300

    Create: Classe Queue criada Update: Classe stack.js renomeada para Stack.js

---
+++ model/Queue.js
@@ -0,0 +1,58 @@
+function Queue() {
+  // Atributos Privados
+  let count = 0
+  let first = 0
+  let items = {}
+
+  //#region Métodos Publicos
+  this.enqueue = _element => {
+    items[count] = _element
+  }
+
+  this.dequeue = () => {
+    if (this.isEmpty()) {
+      return undefined
+    }
+
+    let result = items[first]
+    delete items[first]
+    first++
+    return result
+  }
+
+  this.isEmpty = () => this.size() === 0
+
+  this.front = () => {
+    if (this.isEmpty()) {
+      return undefined
+    }
+
+    items[first]
+  }
+
+  this.size = () => count - first
+
+  this.clear = () => {
+    if (this.isEmpty()) {
+      return 'Pilha já está vazia'
+    }
+
+    while(!this.isEmpty()){
+      this.dequeue()
+    }
+  }
+
+  this.toString = () => {
+    if (this.isEmpty()) {
+      return ''
+    }
+
+    let objString = `${items[first]}`
+
+    for (let i = 0; i < count; i++) {
+      objString = `${objString}${items[i]}`
+    }
+    return objString
+  }
+  //#endregion
+}
