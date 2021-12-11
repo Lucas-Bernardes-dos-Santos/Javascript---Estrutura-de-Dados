b4dbaa6bdea6f7c5d288cb13aa79c43b36086875
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-12-11 13:37:35 -0300

    Update: Linked List.js, Node.js 	Base da Lista criada

---
+++ model/Linked List.js
@@ -0,0 +1,10 @@
+function LinkedList() {
+  // Atributos Privados
+  let count = 0
+  let head = undefined
+
+  // Métodos Públicos
+  this.equals = (_a, _b) => _a === _b
+
+  
+}
---
+++ model/Node.js
@@ -0,0 +1,8 @@
+function Node(_element) {
+  let element = _element
+  let next = undefined
+}
+
+module.exports = {
+  Node
+}
