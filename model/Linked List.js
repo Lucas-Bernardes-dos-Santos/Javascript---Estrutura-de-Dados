6f025281cc30c8f9865ae4227af0bf283740b398
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-12-16 13:52:05 -0300

    Update: Linked List.js 	Agora o head do objeto LinkedList se inicia com um nó vazio

--- model/Linked List.js
+++ model/Linked List.js
@@ -3,7 +3,7 @@ const Node = require('./Node')
 function LinkedList() {
   // Atributos Privados
   let count = 0
-  let head = undefined
+  let head = new Node.Node()
 
   //#region Métodos Públicos
   this.equals = (_a, _b) => _a === _b
@@ -12,7 +12,7 @@ function LinkedList() {
     const node = new Node.Node(_element)
     let current = node
 
-    if(head === undefined) {
+    if(head.element === undefined) {
       head = node
     }
     else {
@@ -110,7 +110,7 @@ function LinkedList() {
   }
 
   this.toString = () => {
-    if(head == null) {
+    if(head.element === undefined) {
       return ''
     }
 
@@ -128,3 +128,40 @@ function LinkedList() {
 module.exports = {
   LinkedList
 }
+
+const ListaEncadead = new LinkedList()
+
+console.log(ListaEncadead.isEmpty())
+
+ListaEncadead.push(0)
+ListaEncadead.push(1)
+ListaEncadead.push(2)
+ListaEncadead.push(3)
+ListaEncadead.push(4)
+
+console.log(ListaEncadead.removeAt(3))
+console.log(ListaEncadead.removeAt(0))
+
+console.log(ListaEncadead.getNodeAt(0))
+console.log(ListaEncadead.getNodeAt(1))
+console.log(ListaEncadead.getNodeAt(2))
+
+console.log(ListaEncadead.pushAt(0, 0))
+console.log(ListaEncadead.pushAt(3, 3))
+console.log(ListaEncadead.pushAt(5, 5))
+console.log(ListaEncadead.pushAt(1.5, 2))
+
+console.log(ListaEncadead.indexOf(1.5))
+console.log(ListaEncadead.indexOf(0))
+console.log(ListaEncadead.indexOf(5))
+
+console.log(ListaEncadead.toString());
+
+console.log(ListaEncadead.remove(1.5))
+console.log(ListaEncadead.remove(5))
+
+console.log(ListaEncadead.size())
+console.log(ListaEncadead.isEmpty())
+console.log(ListaEncadead.getHead())
+
+ListaEncadead.clear()
