027b7f6a9f5ca0fe0301db092d89941675ca5e9e
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-12-30 10:46:00 -0300

    Create: DoubleLinkedList.js, DoubleNode.js 	Criada a estrutura da Lista Duplamente Encadeada

---
+++ model/DoubleLinkedList.js
@@ -0,0 +1,122 @@
+const DoubleNode = require('./DoubleNode')
+
+function DoubleLinkedList() {
+  // Atributos Privados
+  let count = 0
+  let head = new DoubleNode.DoubleNode()
+  let tail = new DoubleNode.DoubleNode()
+
+  //#region Métodos Públicos
+  this.equals = (_a, _b) => _a === _b
+
+  this.pushAt = (_index, _element) => {
+    if(_index >= 0 && _index <= count) {
+      // Criando um nó com o elemento passado
+      const doubleNode = new DoubleNode.DoubleNode(_element)
+
+      // Recebendo o endereço do primeiro elemento
+      let current = head
+
+      // Caso o novo elemento vá ser inserido na início da lista
+      if(_index === 0){
+        // Se head estiver vazio
+        if(head.element === undefined) {
+          head = doubleNode
+          tail = doubleNode
+        }
+        // Caso head não esteja vazio
+        else { 
+          doubleNode.next = head
+          current.previous = doubleNode
+          head = doubleNode
+        }
+      }
+      // Caso o novo elemento vá ser inserido no final da lista
+      else if(_index === count) {
+        current = tail
+        current.next = doubleNode
+        doubleNode.previous = current
+        tail = doubleNode
+      }
+      // Caso o novo elemento vá ser inserido no meio da lista
+      else {
+        // Pegando o endereço da posição anterior a onde o novo elemento será inserido
+        let aux_previous = this.getNodeAt(_index - 1)
+        // Pegando o elemento a frente, local a onde o novo elemento será inserido
+        current = aux_previous.next 
+
+        doubleNode.next = current // O novo elemento.next está agora apontando para o elemento a frente
+        doubleNode.previous = aux_previous // O novo elemento.previous está agora apontando para o elemento de trás
+
+        current.previous = doubleNode // O elemento que current.previous agora recebe aponta para novo elemento
+        aux_previous.next = doubleNode // O elemento aux_previous.next agora aponta para novo elemento
+      }
+      count++
+      return true
+    }
+    return false
+  }
+
+  this.removeAt = _index => {
+    if(_index >= 0 && _index < count) {
+      let current = head
+
+      // Remover o primeiro elemento
+      if(_index === 0) {
+        head = current.next
+
+        // Caso haja apenas um elemento na lista
+        if(count === 1) {
+          tail = undefined
+        }
+        else {
+          head.previous = undefined // Como o primeiro foi removido o próximo elemente se tornará o primeiro e seus previous deixará de apontar para o antigo head
+        }
+      }
+      // Remover o último elemento da lista
+      else if(_index === count - 1) {
+        current = tail
+        tail = current.previous
+        tail.next = undefined
+      }
+      // Remover um elemento no meio da lista
+      else {
+        current = this.getNodeAt(_index)
+        let aux_previous = current.previous
+
+        // Vai fazer a ligação do aux_previous com o current.next
+        aux_previous.next = current.next
+        current.next.previous = aux_previous
+      }
+      count--
+      return current
+    }
+    return undefined
+  }
+
+  this.getNodeAt = _index => {
+    if(_index >= 0 && _index < count){
+      let doubleNode = head
+
+      for (let i = 0; i < _index && doubleNode != null; i++) {
+        doubleNode = doubleNode.next    
+      }
+      return doubleNode
+    }
+    return undefined
+  }
+  //#endregion
+}
+
+let doubleLinkedList = new DoubleLinkedList()
+
+doubleLinkedList.pushAt(0, 0)
+doubleLinkedList.pushAt(1, 1)
+doubleLinkedList.pushAt(1, 0.5)
+
+console.log(doubleLinkedList.getNodeAt(0))
+console.log(doubleLinkedList.getNodeAt(1))
+console.log(doubleLinkedList.getNodeAt(2))
+
+doubleLinkedList.removeAt(1)
+console.log(doubleLinkedList.getNodeAt(1))
---
+++ model/DoubleNode.js
@@ -0,0 +1,9 @@
+function DoubleNode(_element = undefined, _previous = undefined, _next = undefined) {
+  this.element = _element
+  this.previous = _previous
+  this.next = _next
+}
+
+module.exports = {
+  DoubleNode
+}
